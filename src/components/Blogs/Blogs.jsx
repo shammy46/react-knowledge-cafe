827604438652080fc7bea6f263f6b02e61d69868
shammy46 <sanjidashammy81@gmail.com>
2024-03-15 01:18:01 +0600

    project setup

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+const Blogs = () => {
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data));
+        
+}, []);
+
+
+return (
+    <div>
+
+    </div>
+);
+};
+
+export default Blogs;
